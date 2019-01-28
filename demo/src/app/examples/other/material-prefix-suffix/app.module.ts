10cdedba116a57d95437ac747ebff59476b1ba97
Bilal Haidar <bhaidar@gmail.com>
2019-01-28 14:48:10 +0200

    docs: add an example for material suffix and prefix

---
+++ demo/src/app/examples/other/material-prefix-suffix/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+
+import { AppComponent } from './app.component';
+import { FormlyWrapperAddons } from './addons';
+import { addonsExtension } from './addon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormlyMaterialModule,
+    FormlyModule.forRoot({
+      wrappers: [
+        { name: 'addons', component: FormlyWrapperAddons },
+      ],
+      extensions: [
+        { name: 'addons', extension: { onPopulate: addonsExtension } },
+      ],
+    }),
+  ],
+  declarations: [
+    AppComponent,
+    FormlyWrapperAddons,
+  ],
+})
+export class AppModule { }
---
+++ demo/src/app/examples/other/material-prefix-suffix/config.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule, ExamplesRouterViewerComponent } from '../../../shared';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    AppModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ExamplesRouterViewerComponent,
+        data: {
+          examples: [{
+            title: 'Material Prefix and Suffix',
+            description: `
+              This demonstrates adding a material suffix and prefix for material form fields.
+            `,
+            component: AppComponent,
+            files: [
+              { file: 'app.component.html', content: require('!!prismjs-loader?lang=html!./app.component.html'), filecontent: require('!!raw-loader?lang=html!./app.component.html') },
+              { file: 'app.component.ts', content: require('!!prismjs-loader?lang=typescript!./app.component.ts'), filecontent: require('!!raw-loader?lang=typescript!./app.component.ts') },
+              { file: 'addons.ts', content: require('!!prismjs-loader?lang=typescript!./addons.ts'), filecontent: require('!!raw-loader?lang=typescript!./addons.ts') },
+              { file: 'addon.ts', content: require('!!prismjs-loader?lang=typescript!./addon.ts'), filecontent: require('!!raw-loader?lang=typescript!./addon.ts') },
+              { file: 'app.module.ts', content: require('!!prismjs-loader?lang=typescript!./app.module.ts'), filecontent: require('!!raw-loader?lang=typescript!./app.module.ts') },
+            ],
+          }],
+        },
+      },
+    ]),
+  ],
+  entryComponents: [AppComponent],
+})
+export class ConfigModule { }
